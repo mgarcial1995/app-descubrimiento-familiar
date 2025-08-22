34acdacf6731b6438928d0c0582bc59c35256507
Martin <martingarcial-18@hotmail.com>
2025-08-22 10:14:43 -0500

    configuracion con api en nub

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:4000/api",
+//   baseURL: "http://localhost:4000/api",
+  baseURL: "https://api-descubrimiento-familiar.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
